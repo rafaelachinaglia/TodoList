a86e6e8877b668f41cba28ceac47f68854e6e067
Rafaela Chinaglia Ferreira <93222047+rafaelachinaglia@users.noreply.github.com>
2023-02-01 12:18:00 -0300

    Adiciona tarefas

---
+++ TodoList/BlazorApp1/Pages/Todo.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp1.Pages
+{
+    public partial class Todo
+    {
+        private string? newTodo;
+
+        private List<TodoItem> todos = new();
+
+        private void Add()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+
+    }
+}
